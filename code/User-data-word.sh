315af471557e7390a6a22f6917c584bb580a235a
Gian Pedro <devgianpedro@gmail.com>
2025-09-09 11:01:16 -0300

    Update User-data-word.sh

--- code/User-data-word.sh
+++ code/User-data-word.sh
@@ -41,6 +41,7 @@ EOF
 # Inicia os containers
 sudo docker-compose -f /home/ubuntu/docker-compose.yml up -d
 # Esse bloco prepara o Ubuntu para ter acesso ao repositório correto e instala o cliente oficial EFS da AWS
+
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y universe
 sudo apt update -y
@@ -48,13 +49,16 @@ sudo apt install -y amazon-efs-utils
 
 
 # Cria ponto de montagem
+
 sudo mkdir -p /efs
 
 # Monta o EFS com TLS
-sudo mount -t efs -o tls fs-07b069c242fca8f20:/ /efs
+
+sudo mount -t efs -o tls ${efs:id}:/ /efs
 
 # Opcional: adiciona ao fstab para montar automaticamente no boot
-echo "fs-07b069c242fca8f20:/ /efs efs _netdev,tls 0 0" | sudo tee -a /etc/fstab
+
+echo "${efs:id}:/ /efs efs _netdev,tls 0 0" | sudo tee -a /etc/fstab
 
 # esperar algums minutos para efs se conctar 
 # mount | grep efs Para testar a coneção 
