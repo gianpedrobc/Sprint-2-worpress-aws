013c5b0df516d30281549c60f7120651bec47aed
Gian Pedro <devgianpedro@gmail.com>
2025-09-09 09:19:03 -0300

    Update User-data-word.sh

--- code/User-data-word.sh
+++ code/User-data-word.sh
@@ -40,10 +40,9 @@ EOF
 
 # Inicia os containers
 sudo docker-compose -f /home/ubuntu/docker-compose.yml up -d
-
-# Instala utilitários necessários para montar EFS via TLS
+# Esse bloco prepara o Ubuntu para ter acesso ao repositório correto e instala o cliente oficial EFS da AWS
 sudo apt install -y software-properties-common
-sudo add-apt-repository universe
+sudo add-apt-repository -y universe
 sudo apt update -y
 sudo apt install -y amazon-efs-utils
 
@@ -52,10 +51,10 @@ sudo apt install -y amazon-efs-utils
 sudo mkdir -p /efs
 
 # Monta o EFS com TLS
-sudo mount -t efs -o tls ${FS-ID}:/ /efs
+sudo mount -t efs -o tls fs-07b069c242fca8f20:/ /efs
 
-# adiciona ao fstab para montar automaticamente no boot
-echo "${FS-ID:/ /efs efs _netdev,tls 0 0}" | sudo tee -a /etc/fstab
+# Opcional: adiciona ao fstab para montar automaticamente no boot
+echo "fs-07b069c242fca8f20:/ /efs efs _netdev,tls 0 0" | sudo tee -a /etc/fstab
 
-
-# WORDPRESS_CONFIG_EXTRA: Config extra para o WordPress reconhecer o ALB como URL base
+# esperar algums minutos para efs se conctar 
+# mount | grep efs Para testar a coneção 
